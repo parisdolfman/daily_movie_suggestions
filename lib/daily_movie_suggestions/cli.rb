999cf331fe22f91b2057317ffeda24c3d12d2d79
Paris Dolfman <32081304+parisdolfman@users.noreply.github.com>
2020-01-28 13:19:42 -0500

    Stubbed out further for movie_details and fixed case statement

--- lib/daily_movie_suggestions/cli.rb
+++ lib/daily_movie_suggestions/cli.rb
@@ -3,16 +3,35 @@ class DailyMovieSuggestions::CLI
   def call
     list_movies
     movie_details
+    goodbye
   end
 
   def list_movies
     puts "Here are your movie suggestions for today:"
     #get movies from site with best movies
+    puts "1) Movie One, 2) Movie Two, 3) Movie Three"
   end
 
   def movie_details
-    puts "Enter the number of the movie you would like more information about:"
+    puts "Enter the number of the movie you would like more information about or type exit:"
     #get further information about selected movie from site
+    input = nil
+    while input != "exit"
+      input = gets.strip
+
+      case input
+      when "1"
+        puts "Movie One Info"
+      when  "2"
+        puts "Movie Two Info"
+      when "3"
+        puts "Movie Three Info"
+      end
+    end
+  end
+
+  def goodbye
+    puts "Until next time, goodbye!"
   end
 
 end
