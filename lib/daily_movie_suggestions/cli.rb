bd98591f58268595aaa061b9d75a153cff3cc466
Paris Dolfman <32081304+parisdolfman@users.noreply.github.com>
2020-01-28 12:57:40 -0500

    Changed opening line to belong to list_movies rather than call

--- lib/daily_movie_suggestions/cli.rb
+++ lib/daily_movie_suggestions/cli.rb
@@ -1,17 +1,18 @@
 class DailyMovieSuggestions::CLI
 
   def call
-    puts "Here are your movie suggestions for today:" #stub out of beginning of CLI
     list_movies
     movie_details
   end
 
   def list_movies
+    puts "Here are your movie suggestions for today:"
     #get movies from site with best movies
   end
 
   def movie_details
-    puts "Enter the number of the movie you would like more information about"
+    puts "Enter the number of the movie you would like more information about:"
+    #get further information about selected movie from site
   end
 
 end
