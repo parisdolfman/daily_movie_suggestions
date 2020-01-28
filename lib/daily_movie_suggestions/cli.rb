716af65aee3016f6a2ef2e0ffea7b8d14189e929
Paris Dolfman <32081304+parisdolfman@users.noreply.github.com>
2020-01-28 12:53:30 -0500

    Added list_movies and movie_details stub out

--- lib/daily_movie_suggestions/cli.rb
+++ lib/daily_movie_suggestions/cli.rb
@@ -3,10 +3,15 @@ class DailyMovieSuggestions::CLI
   def call
     puts "Here are your movie suggestions for today:" #stub out of beginning of CLI
     list_movies
+    movie_details
   end
 
   def list_movies
     #get movies from site with best movies
+  end
+
+  def movie_details
+    puts "Enter the number of the movie you would like more information about"
   end
 
 end
