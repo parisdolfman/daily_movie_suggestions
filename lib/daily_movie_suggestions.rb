494dcef4f1ed905c92d28b90b451e281a27cfc0c
Paris Dolfman <32081304+parisdolfman@users.noreply.github.com>
2020-02-07 12:07:22 -0500

    Added initialize method

--- lib/daily_movie_suggestions.rb
+++ lib/daily_movie_suggestions.rb
@@ -1,6 +1,11 @@
-require_relative "./daily_movie_suggestions/version"
-require_relative './daily_movie_suggestions/cli' 
+#require_relative "./daily_movie_suggestions/version"
+#require_relative './daily_movie_suggestions/cli'
 
+
+
+module DailyMovieSuggestions
+end
+require_relative '../config/environment'
 #module DailyMovieSuggestions
 #  class Error < StandardError; end
   # Your code goes here...
