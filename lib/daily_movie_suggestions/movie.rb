68d19b8c783c18dd639c7dd16ea188eca534f318
Paris Dolfman <32081304+parisdolfman@users.noreply.github.com>
2020-02-07 12:08:08 -0500

    Added nokogiri

--- lib/daily_movie_suggestions/movie.rb
+++ lib/daily_movie_suggestions/movie.rb
@@ -1,8 +1,23 @@
+require 'pry'
 class DailyMovieSuggestions::Movie
 
 attr_accessor :title, :year, :rating, :url
 
 @@all = []
+
+def initialize(title, year, rating, url)
+  @title = title
+  @year = year
+  @rating = rating
+  @url = url
+  @@all << self
+end
+
+def doc
+  doc = Nokogiri::HTML(open("https://www.rottentomatoes.com/top/bestofrt/"))
+binding.pry
+end
+
 
 
 
