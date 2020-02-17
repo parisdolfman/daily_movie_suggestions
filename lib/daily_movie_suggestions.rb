e59bb67896838cbc25d309f019f76bf7e72a02d4
Paris Dolfman <32081304+parisdolfman@users.noreply.github.com>
2020-02-17 14:21:59 -0500

    Updated require relative

--- lib/daily_movie_suggestions.rb
+++ lib/daily_movie_suggestions.rb
@@ -1,11 +1,12 @@
-#require_relative "./daily_movie_suggestions/version"
-#require_relative './daily_movie_suggestions/cli'
-
-
-
+require_relative './daily_movie_suggestions/version'
+require_relative './daily_movie_suggestions/cli'
+require_relative './daily_movie_suggestions/movie'
+require_relative './daily_movie_suggestions/scraper'
+require_relative '..lib/daily_movie_suggestions/cli'
+require_relative '..lib/daily_movie_suggestions/version'
 module DailyMovieSuggestions
 end
-require_relative '../config/environment'
+#require_relative '../config/environment'
 #module DailyMovieSuggestions
 #  class Error < StandardError; end
   # Your code goes here...
