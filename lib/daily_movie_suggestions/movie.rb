93d236229e4b2b7895c0ad105ec5ba4edacc52a0
Paris Dolfman <32081304+parisdolfman@users.noreply.github.com>
2020-02-26 12:29:23 -0500

    Fixed rating variable to return correct string

--- lib/daily_movie_suggestions/movie.rb
+++ lib/daily_movie_suggestions/movie.rb
@@ -13,12 +13,13 @@ def self.today
   doc = Nokogiri::HTML(open("https://www.rottentomatoes.com/top/bestofrt/"))
 
   @title = doc.search("table.table a.unstyled.articleLink")[1].text
+  @rating = doc.search("table.table span.tMeterScore")[1].text.split(//).last(3).join
   binding.pry
 end
 
 
 def rating
-  @rating = doc.search("table.table span.tMeterScore").text
+  @rating = doc.search("table.table span.tMeterScore")[1].text
 end
 
 
