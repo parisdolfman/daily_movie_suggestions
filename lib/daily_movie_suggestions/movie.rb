b7aacdb37c69b6fd09dab8108ac951d410940547
Paris Dolfman <32081304+parisdolfman@users.noreply.github.com>
2020-04-01 15:28:43 -0400

    Working to resolve rating issue

--- lib/daily_movie_suggestions/movie.rb
+++ lib/daily_movie_suggestions/movie.rb
@@ -12,32 +12,4 @@ def self.all
   @@all
 end
 
-def self.today
-  self.scrape_rt
-end
-
-def self.scrape_rt
-
-  doc = Nokogiri::HTML(open("https://www.rottentomatoes.com/top/bestofrt/"))
-
-  @movie_list = doc.search("table.table a.unstyled.articleLink")
-  @movie_list.each.with_index(1) do |movie, i|
-    @new_movie = DailyMovieSuggestions::Movie.new
-    @new_movie.title = movie.text.strip
-    @new_movie.number = i
-      if i < 4
-      puts "#{@new_movie.number}. #{@new_movie.title}"
-      end
-      @new_movie.save
-    end 
-  end
-
-# def self.details
-#   @@all.each do |movie|
-#     @movie = movie
-#     if movie.title
-#
-# end
-
-
 end
