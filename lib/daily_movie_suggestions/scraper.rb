14606b96e29cba579c7f0a74e821ab73e15b4f88
Paris Dolfman <32081304+parisdolfman@users.noreply.github.com>
2020-04-01 15:30:41 -0400

    Working to resolve rating issue

--- lib/daily_movie_suggestions/scraper.rb
+++ lib/daily_movie_suggestions/scraper.rb
@@ -1,10 +1,38 @@
 class DailyMovieSuggestions::Scraper
 
-def self.get_page
-  doc = Nokogiri::HTML(open("https://www.rottentomatoes.com/top/bestofrt/"))
-  binding.pry
-end
 
+def self.movie_titles
+
+  doc = Nokogiri::HTML(open("https://www.rottentomatoes.com/top/bestofrt/"))
+
+  @movie_list = doc.search("table.table a.unstyled.articleLink")
+  @movie_list.each.with_index(1) do |movie, i|
+    @new_movie = DailyMovieSuggestions::Movie.new
+    @new_movie.title = movie.text.strip
+    @new_movie.number = i
+      if i < 4
+      puts "#{@new_movie.number}. #{@new_movie.title}"
+      end
+      @new_movie.save
+    end
+  end
+
+
+  def self.rating
+
+    doc = Nokogiri::HTML(open("https://www.rottentomatoes.com/top/bestofrt/"))
+
+    @movie_list = doc.search("table.table span.tMeterScore")
+    @movie_list.each.with_index(1) do |rating, i|
+      @new_movie = DailyMovieSuggestions::Movie.new
+      @new_movie.rating = rating
+       @new_movie.number = i
+        if i < 4
+        puts "#{@new_movie.number}. #{@new_movie.rating}"
+        end
+        @new_movie.save
+      end
+    end
 
 
 end
