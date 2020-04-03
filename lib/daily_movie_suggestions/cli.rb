065b1046a565cf42a099f049cc16642a19ba004e
Paris Dolfman <32081304+parisdolfman@users.noreply.github.com>
2020-04-03 15:12:54 -0400

    Removed unnecessary attr_accessor

--- lib/daily_movie_suggestions/cli.rb
+++ lib/daily_movie_suggestions/cli.rb
@@ -8,12 +8,15 @@ class DailyMovieSuggestions::CLI
   def list_movies
     puts "Here are your movie suggestions for today:"
     DailyMovieSuggestions::Scraper.movie_titles
+    puts "          "
+    puts "Would you like to view the ratings for these movies?"
   end
 
   def menu
+    puts "          "
     menu = puts "Command Menu:"
-    puts "- Enter the number of the movie you would like to know the rating for: 1, 2, or 3"
-    puts "- Enter list again to view the list again"
+    puts "- Enter yes to view the ratings for each movie"
+    puts "- Enter list again to view the movie list again"
     puts "- Enter exit to leave"
     puts menu
   end
@@ -25,29 +28,22 @@ class DailyMovieSuggestions::CLI
     while input != "exit"
       input = gets.strip
 
-      if input.to_s == "1" || input.to_s == "2" || input.to_s == "3"
+      if input.downcase == "yes"
         DailyMovieSuggestions::Scraper.rating
-
-
-
-
       elsif input == "list again"
         list_movies
       elsif input == "exit"
         goodbye
       else
+        puts "          "
         puts "I'm not sure what you mean by that. Please enter a valid command from the menu."
         menu
       end
-      end
     end
-
-
-
+  end
 
   def goodbye
     puts "Until next time, goodbye!"
   end
-
 
 end
